cc67ae43f13bccc4e40e95bf1bf1b2775323943c
wangwei <532300391@qq.com>
2023-08-14 09:38:42 +0800

    add format for CZMLIntervalValue

--- src/RJSFCustom/CZMLIntervalValue.tsx
+++ src/RJSFCustom/CZMLIntervalValue.tsx
@@ -1,36 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { DatePicker } from 'antd';
+import dayjs from 'dayjs';
+// dayjs.locale('zh-cn');
+dayjs().locale('zh-cn').format();
 
 function DoubleDateTimePicker(props) {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+	const value = props.value;
+	let start = null;
+	let end = null;
+	if (value) {
+		start = value.split('/')[0]
+		end = value.split('/')[1]
+		start = new dayjs(start);
+		end = new dayjs(end);
+	}
 
-	useEffect(()=>{
-    if (startDate && endDate) {
+	const [startDate, setStartDate] = useState(start);
+	const [endDate, setEndDate] = useState(end);
+
+	useEffect(() => {
+		if (startDate && endDate) {
 			const res = `${startDate.toISOString()}/${endDate.toISOString()}`
-			console.log(' res --- ', res);
-      props.onChange(res);
-    }
+			props.onChange(res);
+		}
 	}, [startDate, endDate])
 
-  return (
-    <div>
-      <DatePicker 
-        value={startDate} 
-        onChange={(date) => { 
-          setStartDate(date);
-        }} 
+	return (
+		<div>
+			<DatePicker
+				value={startDate}
+				onChange={(date) => {
+					setStartDate(date);
+				}}
 				showTime
-      />
-      <DatePicker 
-        value={endDate} 
-        onChange={(date) => { 
-          setEndDate(date);
-        }} 
+			/>
+			<DatePicker
+				value={endDate}
+				onChange={(date) => {
+					setEndDate(date);
+				}}
 				showTime
-      />
-    </div>
-  );
+			/>
+		</div>
+	);
 }
 
 export default DoubleDateTimePicker;
